efa9585f60bd2985c4a790ff72b85b9a1d116e49
weis <578402809@qq.com>
2019-09-26 18:09:27 +0800

    delete groupView

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models  import User, Group
 from rest_framework import viewsets
-from .serializers import UserSerializer, GroupSerializer
+from .serializers import UserSerializer
 from rest_framework.generics import ListAPIView, ListCreateAPIView,CreateAPIView
 
 
@@ -9,10 +9,4 @@ class Register(CreateAPIView):
     serializer_class = UserSerializer
 
     
-    
-
-class GroupViewSet(viewsets.ModelViewSet):
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
-
     
